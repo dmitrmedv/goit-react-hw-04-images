7db5be0bde677557c3f3eead352249012992628a
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2023-09-18 15:14:43 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,34 @@
-// import css from './Modal.module.css';
-// import { createPortal } from 'react-dom';
-// import { useEffect, useRef } from 'react';
+import css from './Modal.module.css';
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 
-// const modalRoot = document.querySelector('#modal-root');
+const modalRoot = document.querySelector('#modal-root');
 
-const Modal = () => {
-  // componentDidMount() {
-  //   window.addEventListener('keydown', this.escCloseModal);
-  // }
+const Modal = ({ largeImageURL, toggleModal }) => {
+  useEffect(() => {
+    function escCloseModal(e) {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    }
+    window.addEventListener('keydown', escCloseModal);
+    return () => window.removeEventListener('keydown', escCloseModal);
+  }, [toggleModal]);
 
-  // componentWillUnmount() {
-  //   window.removeEventListener('keydown', this.escCloseModal);
-  return;
+  const closeOnBackDrop = event => {
+    if (event.target === event.currentTarget) {
+      toggleModal();
+    }
+  };
+
+  return createPortal(
+    <div className={css.overlay} onClick={closeOnBackDrop}>
+      <div className={css.modal}>
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 };
-
-// const escCloseModal = e => {
-//   if (e.code === 'Escape') {
-//     this.props.toggleModal();
-//   }
-// };
-
-// const closeOnBackDrop = event => {
-//   if (event.target === event.currentTarget) {
-//     this.props.toggleModal();
-//   }
-// };
-
-// return createPortal(
-//   <div className={css.overlay} onClick={closeOnBackDrop}>
-//     <div className={css.modal}>
-//       <img src={this.props.largeImageURL} alt="" />
-//     </div>
-//   </div>,
-//   modalRoot
-// );
 
 export default Modal;
